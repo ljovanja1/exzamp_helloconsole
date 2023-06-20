0101761a1e969945c6f036f773b930155c59f8d6
roman <you@example.com>
2023-06-20 21:17:05 +0300

    exzamp5

--- exzamp5/Program.cs
+++ exzamp5/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "roman")
+{
+    Console.WriteLine("Ура, этоже Roma");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
