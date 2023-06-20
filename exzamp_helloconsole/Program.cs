13c6c4252cbf190f782dcae858cadae75aceee75
roman <you@example.com>
2023-06-20 19:33:13 +0300

    add new folder

---
+++ exzamp_helloconsole/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World");
+Console.WriteLine("dotnet console - создать новый проект");
+Console.WriteLine("dotnet run - запустить проект");
+Console.WriteLine("Hello, Roman");
